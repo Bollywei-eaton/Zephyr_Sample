13e1dfc2f5ff6f048323691d71efb904762e6ff0
Bolly <Bollywei@eaton.com>
2023-06-08 14:41:40 +0800

    Use another way to operate gpio dev

--- blinky/src/main.c
+++ blinky/src/main.c
@@ -12,42 +12,73 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
-#define BUTTON_NODE DT_ALIAS(sw0)
-
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct gpio_dt_spec sw = GPIO_DT_SPEC_GET(BUTTON_NODE, gpios);
+struct led {
+	struct gpio_dt_spec spec;
+	const char *gpio_pin_name;
+};
+
+static const struct led led_0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+	.gpio_pin_name = DT_PROP_OR(LED0_NODE, label, ""),
+};
+
+/* The devicetree node identifier for the "sw0" alias. */
+#define BUTTON_NODE DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(BUTTON_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+
+struct sw {
+	struct gpio_dt_spec spec;
+	const char *gpio_pin_name;
+};
+
+static const struct sw sw_0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(BUTTON_NODE, gpios, {0}),
+	.gpio_pin_name = DT_PROP_OR(BUTTON_NODE, label, ""),
+};
 
 void main(void)
 {	
 	int ret;
 
 	printk("Hello World! %s\n", CONFIG_BOARD);
-	
-	if ((!device_is_ready(led.port)) || (!device_is_ready(sw.port))) {
+
+	if (!device_is_ready(led_0.spec.port)) {
+		printk("Error: %s device is not ready\n", led_0.spec.port->name);
 		return;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (!device_is_ready(sw_0.spec.port)) {
+		printk("Error: %s device is not ready\n", sw_0.spec.port->name);
 		return;
 	}
 
-	ret = gpio_pin_configure_dt(&sw, GPIO_INPUT);
+	ret = gpio_pin_configure_dt(&led_0.spec, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
+		printk("Error %d: failed to configure pin %d (LED '%s')\n",
+			ret, led_0.spec.pin, led_0.gpio_pin_name);		
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&sw_0.spec, GPIO_INPUT);
+	if (ret < 0) {
+		printk("Error %d: failed to configure pin %d (SW '%s')\n",
+			ret, sw_0.spec.pin, sw_0.gpio_pin_name);			
 		return;
 	}	
 
 	while (1) {
-		if( gpio_pin_get_dt(&sw) )
+		if( gpio_pin_get(sw_0.spec.port, sw_0.spec.pin) )
 		{
-			ret = gpio_pin_toggle_dt(&led);
-			if (ret < 0) {
-				return;
-			}
+			gpio_pin_toggle(led_0.spec.port, led_0.spec.pin);
 		}
 		k_msleep(SLEEP_TIME_MS);
 	}
