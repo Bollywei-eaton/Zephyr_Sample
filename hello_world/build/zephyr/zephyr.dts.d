567f4f6728f0e7fc50663c5535f430c66ce51ae1
Bolly <Bollywei@eaton.com>
2023-07-13 14:40:32 +0800

    Add hello world task demo

---
+++ hello_world/build/zephyr/zephyr.dts.d
@@ -0,0 +1,27 @@
+empty_file.o: C:/Project/Targa/zephyrproject/zephyr/misc/empty_file.c \
+ C:/Project/Targa/zephyrproject/zephyr/boards/arm/nucleo_h753zi/nucleo_h753zi.dts \
+ C:/Project/Targa/zephyrproject/zephyr/dts/arm/st/h7/stm32h753Xi.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/dts/common/mem.h \
+ C:/Project/Targa/zephyrproject/zephyr/dts/arm/st/h7/stm32h753.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/dts/arm/st/h7/stm32h743.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/dts/arm/st/h7/stm32h7.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/stm32h7_clock.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/stm32_pwm.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/dma/stm32_dma.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/stm32h7_adc.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/stm32_adc.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/reset/stm32h7_reset.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/reset/stm32-common.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/memory-controller/stm32-fmc-sdram.h \
+ C:/Project/Targa/zephyrproject/zephyr/dts/common/freq.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/display/panel.h \
+ C:/Project/Targa/zephyrproject/modules/hal/stm32/dts/st/h7/stm32h753zitx-pinctrl.dtsi \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl.h \
+ C:/Project/Targa/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl-common.h \
+ C:/Project/Targa/zephyrproject/zephyr/boards/arm/nucleo_h753zi/arduino_r3_connector.dtsi
